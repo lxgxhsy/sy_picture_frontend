f11e67e96c50b5c828fe5328541e70ee98fc8d74
sy <3078567417@qq.com>
2025-02-03 23:53:43 +0800

    第 4 期 - 图片模块 - 前端 1. 图片上传和创建 / 修改页面 2. 图片管理页面 3. 图片列表页面（主页） 4. 图片详情页面 5. 图片下载

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,9 +2,19 @@
 /* eslint-disable */
 // API 更新时间：
 // API 唯一标识：
+import * as fileController from './fileController'
+import * as pictureController from './pictureController'
+import * as spaceController from './spaceController'
+import * as spaceAnalyzeController from './spaceAnalyzeController'
+import * as spaceUserController from './spaceUserController'
 import * as testController from './testController'
 import * as userController from './userController'
 export default {
+  fileController,
+  pictureController,
+  spaceController,
+  spaceAnalyzeController,
+  spaceUserController,
   testController,
   userController,
 }
